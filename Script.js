807d7c7e0c889bf3f641557ad9c1cbbb7349d21c
yeswanthnarayana <164862246+yeswanthnarayana@users.noreply.github.com>
2025-12-27 11:56:49 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,8 +1,36 @@
-document.querySelectorAll('.profile').forEach(profile => {
-profile.addEventListener('mouseenter', () => {
-profile.style.backgroundColor = '#f0f0f0';
-});
-profile.addEventListener('mouseleave', () => {
-profile.style.backgroundColor = 'white';
-});
-});
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+
+// USING ASYNC/AWAIT
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  }
+
+  const res = await fetch('https://icanhazdadjoke.com', config)
+
+  const data = await res.json()
+
+  jokeEl.innerHTML = data.joke
+}
+
+// USING .then()
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: 'application/json',
+//     },
+//   }
+
+//   fetch('https://icanhazdadjoke.com', config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       jokeEl.innerHTML = data.joke
+//     })
+// }
